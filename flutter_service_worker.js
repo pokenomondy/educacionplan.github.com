1271650bca608fb99d302b95ef8aba4e2f15dc08
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2023-10-14 11:08:30 -0500

    modificacion inicial

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0dcb142e2caa101e493ef40aed03e394",
-"/": "0dcb142e2caa101e493ef40aed03e394",
-"main.dart.js": "1fdd128126cb67c107a9af222f641d2f",
+"index.html": "970001e295f99b2dd4d87d4d6e714be7",
+"/": "970001e295f99b2dd4d87d4d6e714be7",
+"main.dart.js": "5331bb5960e6a5d40a160737fb4a8793",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
