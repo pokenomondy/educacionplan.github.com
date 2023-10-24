6f95476b722377945b04c4793f8770664f73c8d3
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2023-10-24 15:56:32 -0500

    Tutores DASH ENTREGAS

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b50ba2a599078329b4df0a6372b8decf",
-"/": "b50ba2a599078329b4df0a6372b8decf",
-"main.dart.js": "06ce9b1467c5b7844e712f03fb3baccb",
+"index.html": "a02372fa6a38958e075c92244e2dbcee",
+"/": "a02372fa6a38958e075c92244e2dbcee",
+"main.dart.js": "e21459af174c52570ec4383ca725c129",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
