783ac3053800246f8c60b61bfe0139e260a2b7e5
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2023-10-25 18:21:47 -0500

    Plugins y segurida v2
    Se arreglan problemas ocurridos

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e40b0ffb107e70d94cf59845cd5ee349",
-"/": "e40b0ffb107e70d94cf59845cd5ee349",
-"main.dart.js": "a0dd985a6c8a0911c685fc99ee845033",
+"index.html": "affd9673261e02c7cd9c3660fb7b106b",
+"/": "affd9673261e02c7cd9c3660fb7b106b",
+"main.dart.js": "7fe39610587d4cf28435e2b2c8ce1053",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
