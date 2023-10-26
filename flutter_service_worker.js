6dd95a54a573d3e68f6b9dc2b22033fa04405968
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2023-10-25 21:15:10 -0500

    Quitado botón de login
    Quitado botón de login

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "affd9673261e02c7cd9c3660fb7b106b",
-"/": "affd9673261e02c7cd9c3660fb7b106b",
-"main.dart.js": "7fe39610587d4cf28435e2b2c8ce1053",
+"index.html": "38cd2e4873ef73c39aa451cbc9980bd5",
+"/": "38cd2e4873ef73c39aa451cbc9980bd5",
+"main.dart.js": "46c17dcc2759f06f6acedfbde631d60a",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
