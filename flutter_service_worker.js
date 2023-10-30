76184f86f7f74902f9ffbd2cc501ac0f8b41c1a9
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2023-10-30 10:14:26 -0500

    Verificadores de información
    Verificadores de actualizar solicitudes cada cierto tiempo
    Verificadores de actualziar tutores cada cierto tiempo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "397c86c7eccaefdbbc4e4328b56bfca6",
-"/": "397c86c7eccaefdbbc4e4328b56bfca6",
-"main.dart.js": "1d5f20c25922bd8c40294fa5639438ca",
+"index.html": "d900e19a5fc9f504763ceea6b8c6d538",
+"/": "d900e19a5fc9f504763ceea6b8c6d538",
+"main.dart.js": "4bdaed7288f575e01b488081e5e28db3",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
