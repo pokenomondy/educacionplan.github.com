5ee923f5a4dcc43d85ab51589acdc2585dd6d557
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2023-11-12 13:56:03 -0500

    V 1.8 PAGOS INTEGRADOS

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c7a82ee2692a35699bfc305aab06828d",
-"/": "c7a82ee2692a35699bfc305aab06828d",
-"main.dart.js": "f55a265453849d36250abd7faf15c2bc",
+"index.html": "af0de920ef27c37cbeb29395e2bbc864",
+"/": "af0de920ef27c37cbeb29395e2bbc864",
+"main.dart.js": "a7aace2eaa8a68579f6efcd38cf69988",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
