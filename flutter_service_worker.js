83f7987a911cad27058d2840d977d46477927d70
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2023-11-29 17:25:39 -0500

    Fix clientes, fix calendario, varias cosit
    v 1.0-17
    
    Se adiciona editar clientes
    se adicionan cosas adicionales, version completa y chevere

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544",
 "assets/AssetManifest.json": "92595af3477a4a752167f9dcb58d6463",
 "assets/FontManifest.json": "ce1d0ae664e14238869feff07c71ce21",
-"assets/fonts/MaterialIcons-Regular.otf": "19ac2a46f6123edf7cb3f7f3ae00baa6",
+"assets/fonts/MaterialIcons-Regular.otf": "b47e6f0aeee79669563b40adfbca83a0",
 "assets/NOTICES": "e956dbaf6edfe7461eabeb062832c068",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/packages/fluent_ui/assets/AcrylicNoise.png": "81f27726c45346351eca125bd062e9a7",
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0f472ab96381a114ab7f4b88c92762a2",
-"/": "0f472ab96381a114ab7f4b88c92762a2",
-"main.dart.js": "f547ba8660e1bcf5cdbff0a3a911e726",
+"index.html": "7402f2c5b19d0731024eb7130f679c27",
+"/": "7402f2c5b19d0731024eb7130f679c27",
+"main.dart.js": "5d714091aadf7b4d0a3a17a810ef6c76",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
