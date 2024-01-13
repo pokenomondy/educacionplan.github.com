54fa5a1e00d8cf482a23c364319f0db5f25ca72d
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2024-01-13 18:24:02 -0500

    v 1.0.29
    Versión de tutores
    versión mas aavanzadas
    vamos a probar

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0550b5ed9115f601553229239cfcb89a",
-"/": "0550b5ed9115f601553229239cfcb89a",
-"main.dart.js": "938d859512a5b26cd9309b5a0004a797",
+"index.html": "b954f7cdbbb4e4f7360e7648f21556b3",
+"/": "b954f7cdbbb4e4f7360e7648f21556b3",
+"main.dart.js": "24d6a2fa329f83b8d30f14927e2b4fa8",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
