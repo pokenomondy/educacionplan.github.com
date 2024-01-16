acc1f4b345e34e68faa8267ce3d4592fb26b7dce
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2024-01-16 17:29:56 -0500

    Versión para agregar clientes en dufy
    Version para agregar clientes, vamos a ver

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "266d02652afe8bcf3429cdea8c12d3d2",
-"/": "266d02652afe8bcf3429cdea8c12d3d2",
-"main.dart.js": "a79fbfd260f3591030eaeed1c26fd7f5",
+"index.html": "59cf0f3f30ace33345b952b22bb28aed",
+"/": "59cf0f3f30ace33345b952b22bb28aed",
+"main.dart.js": "8a5d3725961fcbbd24860db61cc5fbc3",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
