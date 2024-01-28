1677125cddc2f6cafb9ce93f48d069f2c0e15a30
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2024-01-28 11:08:46 -0500

    v 1.1.02 deploy dufy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "74bf7dfe9d6c17a041745e03d663e2fc",
-"/": "74bf7dfe9d6c17a041745e03d663e2fc",
-"main.dart.js": "e726cd9a5d7d99d97ab6be04b3870018",
+"index.html": "c98075e8d6d36e7a379e6feff87be456",
+"/": "c98075e8d6d36e7a379e6feff87be456",
+"main.dart.js": "db8005cb1c36e4996765b07b0b7efb35",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
