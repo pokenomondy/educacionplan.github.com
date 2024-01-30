c48503c76d4bce9a43472851dbf474eae184b03d
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2024-01-29 22:27:34 -0500

    v 1.1.04
    Versión fixeada
    
    Bastantes arreglos in app, arreglos basicos y especificos

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "67c998bf338020c866fe4361ee6af822",
-"/": "67c998bf338020c866fe4361ee6af822",
-"main.dart.js": "3258df2a0a3d0e938e5d4d28e56e43cf",
+"index.html": "45764b2e9d9fdaa8a592ad7647385129",
+"/": "45764b2e9d9fdaa8a592ad7647385129",
+"main.dart.js": "4d9308bd3a0d0888d61c50bb0cd08259",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
