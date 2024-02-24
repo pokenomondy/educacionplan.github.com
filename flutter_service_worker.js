787b36d09c5a4da836aa11008072090d77a8d0e2
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2024-02-23 22:03:06 -0500

    v.1.1.11 - fixes camapañs
    campañas

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c2fcf1e79683cd5fac0ad97c580bb67a",
-"/": "c2fcf1e79683cd5fac0ad97c580bb67a",
-"main.dart.js": "1cd71030a70ad10d6b8f23a99eff614c",
+"index.html": "716b9a3588f90f532ee700e333a9907c",
+"/": "716b9a3588f90f532ee700e333a9907c",
+"main.dart.js": "1a6cf3c36fc182702cf06bc3e9bd7264",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
