25e2f392fb652a7ed71f7b83f77a82a2569ebc3c
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2024-03-01 18:55:47 -0500

    version fix whatsapp

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "527bd5af70ebb56affbc2172826ef6af",
-"/": "527bd5af70ebb56affbc2172826ef6af",
-"main.dart.js": "effdd5eb35a14487aba11d9af497eb82",
+"index.html": "1de7f137fd6fc38a64f8454612cfda82",
+"/": "1de7f137fd6fc38a64f8454612cfda82",
+"main.dart.js": "25324fb6c9440474411e1a7c79a6868b",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
